3e4e445da82bd541a8eec803f08c0a50613e5b8e
k.struzhkin <k.struzhkin@rbkadmins-MacBook-Pro.local>
2019-05-15 17:48:28 +0300

    Add builder

--- src/main/java/com/rbkmoney/adapter/bank/spring/boot/starter/model/ExitStateModel.java
+++ src/main/java/com/rbkmoney/adapter/bank/spring/boot/starter/model/ExitStateModel.java
@@ -1,10 +1,12 @@
 package com.rbkmoney.adapter.bank.spring.boot.starter.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ExitStateModel {
