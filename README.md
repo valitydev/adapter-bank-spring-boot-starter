58eee5dbfc8bee5095fee333dc17e9564479c349
Anatoly Karlov <karleowne@gmail.com>
2022-05-17 15:05:53 +0700

    refactor project, update deps

--- README.md
+++ README.md
@@ -1,21 +1,124 @@
 ### adapter-bank-spring-boot-starter
 
-**Adapter-bank-spring-boot-starter** - это подключаемый Spring Boot модуль для стандартных 
-компонентов proxy-алаптеров. 
+**Adapter-bank-spring-boot-starter** - это подключаемый Spring Boot модуль для стандартных компонентов proxy-адаптеров.
 
-Для корректной работы Вашего адаптера необходимо:
-*  подключить необходимые properties (AdapterProperties, TimerProperties). Если поля родителя
-   не покрывают всех необходимых бизнес-полей, то необходимо унаследоваться от родителя и 
-   при помощи аннотаций @Configuration и @ConfigurationProperties создать соответствующий
-   класс конфигураций .Если поля родителя полностью покрывают case, то можно не создавать 
-   класс-наследник, а создать соответствующий bean в конфигурации. Например:
+```yaml
+adapter-bank-spring-boot-starter:
+  object-mapper:
+    enabled: true
+  error-mapping:
+    enabled: true
+  rest-template:
+    enabled: true
+  request-logging-aspect:
+    enabled: true
+  network:
+    enabled: true
+  logging-interceptor:
+    enabled: false
+  hellgate-client:
+    enabled: true
+  cds-storage-client:
+    enabled: true
+  cds-id-storage-client:
+    enabled: true
+  bender-client:
+    enabled: true
+  adapter:
+    enabled: true
+  timer:
+    enabled: true
+  serde:
+    enabled: true
+  processor:
+    enabled: true
+  ctxToEntryModelConverter:
+    enabled: true
+  entryModelToBaseRequestModelConverter:
+    enabled: true
+  exitModelToProxyResultConverter:
+    enabled: true
+  exitModelToRecTokenProxyResultConverter:
+    enabled: true
+  exitStateModelToTemporaryContextConverter:
+    enabled: true
+  exponentialBackOffPollingService:
+    enabled: true
+  intentResultFactory:
+    enabled: true
+  paymentCallbackHandler:
+    enabled: true
+  paymentServerFlowHandler:
+    enabled: true
+  recCtxToEntryModelConverter:
+    enabled: true
+  recurrentIntentResultFactory:
+    enabled: true
+  recurrentResultIntentResolver:
+    enabled: true
+  recurrentTokenCallbackHandler:
+    enabled: true
+  resultIntentResolver:
+    enabled: true
+  serverHandlerLogDecorator:
+    enabled: true
+  stepResolver:
+    enabled: true
+  temporaryContextService:
+    enabled: true
+  threeDsAdapterService:
+    enabled: true
+  tokenServerFlowHandler:
+    enabled: true
 
+adapter:
+  url: http://127.0.0.1
+  callbackUrl: http://127.0.0.1:8080
+  pathCallbackUrl: /${server.rest.endpoint}/term_url
+  pathRecurrentCallbackUrl: /${server.rest.endpoint}/rec_term_url
+  defaultTermUrl: https://checkout.empayre.com/v1/finish-interaction.html
+  tagPrefix: adapter-
 
-    @Bean
-    @Primary
-    @ConfigurationProperties("time.config")
-    public TimerProperties timerProperties() {
-        return new TimerProperties();
-    }
-    
-    
+bender:
+  client:
+    url: http://127.0.0.1:8022/v1/bender
+    networkTimeout: 5000
+    namespace: namespace
+
+cds:
+  client:
+    identity-document-storage:
+      url: http://127.0.0.1:8022/v1/identity_document_storage
+      networkTimeout: 5000
+    storage:
+      url: http://127.0.0.1:8022/v1/storage
+      networkTimeout: 5000
+
+hellgate:
+  client:
+    url: http://127.0.0.1:8022/v1/proxyhost/provider
+    networkTimeout: 30000
+
+time:
+  config:
+    maxTimePollingMin: 60
+    pollingDelayMs: 1000
+    redirectTimeoutMin: 15
+    exponential: 2
+    defaultInitialExponential: 2
+    maxTimeBackOff: 600
+    maxTimeCoefficient: 2
+
+rest-template:
+  requestTimeout: 60000
+  poolTimeout: 10000
+  connectionTimeout: 10000
+  maxTotalPooling: 200
+  defaultMaxPerRoute: 200
+  networkTimeout: 60000
+  connectTimeout: 10000
+
+error-mapping:
+  file: classpath:fixture/errors.json
+  patternReason: "'%s' - '%s'" # 'code' - 'description'
+```
--- src/test/java/dev/vality/adapter/bank/spring/boot/starter/configuration/StarterTest.java
+++ src/test/java/dev/vality/adapter/bank/spring/boot/starter/configuration/StarterTest.java
@@ -1,14 +1,48 @@
 package dev.vality.adapter.bank.spring.boot.starter.configuration;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = {
+        RemoteClientImpl.class,
+        Validator.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.SimpleObjectMapperConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.SimpleErrorMappingConfiguration.class,
+//        dev.vality.adapter.bank.spring.boot.starter.configuration.RestTemplateConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.RequestLoggingAspectConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.NetworkConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.LoggingInterceptorConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.HellgateClientConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.CdsStorageClientConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.CdsIdStorageClientConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.BenderConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.AdapterConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.TimerConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.SerdeConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.ProcessorConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.CtxToEntryModelConverterConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.EntryModelToBaseRequestModelConverterConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.ExitModelToProxyResultConverterConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.ExitModelToRecTokenProxyResultConverterConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.ExitStateModelToTemporaryContextConverterConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.ExponentialBackOffPollingServiceConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.IntentResultFactoryConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.PaymentCallbackHandlerConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.PaymentServerFlowHandlerConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.RecCtxToEntryModelConverterConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.RecurrentIntentResultFactoryConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.RecurrentResultIntentResolverConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.RecurrentTokenCallbackHandlerConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.ResultIntentResolverConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.ServerHandlerLogDecoratorConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.StepResolverConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.TemporaryContextServiceConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.ThreeDsAdapterServiceConfiguration.class,
+        dev.vality.adapter.bank.spring.boot.starter.configuration.TokenServerFlowHandlerConfiguration.class})
+@AutoConfigureWebClient
 @PropertySource("classpath:application.yml")
-@ComponentScan("dev.vality.adapter.bank.spring.boot.starter.configuration")
 public class StarterTest {
 
     @Test
