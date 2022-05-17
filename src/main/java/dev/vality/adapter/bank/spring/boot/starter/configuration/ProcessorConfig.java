313f15764d440c63768d0c68de69e769316c2889
Anatoly Karlov <karleowne@gmail.com>
2022-05-17 11:40:34 +0700

    refactor project, update deps

---
+++ src/main/java/dev/vality/adapter/bank/spring/boot/starter/configuration/ProcessorConfig.java
@@ -0,0 +1,23 @@
+package dev.vality.adapter.bank.spring.boot.starter.configuration;
+
+import dev.vality.adapter.flow.lib.model.BaseResponseModel;
+import dev.vality.adapter.flow.lib.model.EntryStateModel;
+import dev.vality.adapter.flow.lib.model.ExitStateModel;
+import dev.vality.adapter.flow.lib.processor.*;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnExpression("${adapter-bank-spring-boot-starter.processor.enabled:true}")
+public class ProcessorConfig {
+
+    @Bean
+    public Processor<ExitStateModel, BaseResponseModel, EntryStateModel> baseProcessor() {
+        ErrorProcessor errorProcessor = new ErrorProcessor();
+        SuccessFinishProcessor baseProcessor = new SuccessFinishProcessor(errorProcessor);
+        RedirectProcessor redirectProcessor = new RedirectProcessor(baseProcessor);
+        return new RetryProcessor(redirectProcessor);
+    }
+
+}
