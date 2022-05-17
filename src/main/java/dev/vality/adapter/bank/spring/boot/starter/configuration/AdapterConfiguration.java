188fb3521bb294ab60490a7602c19912fc5ce5da
Anatoly Karlov <karleowne@gmail.com>
2022-05-17 11:46:08 +0700

    refactor project, update deps

--- src/main/java/dev/vality/adapter/bank/spring/boot/starter/configuration/AdapterConfiguration.java
+++ src/main/java/dev/vality/adapter/bank/spring/boot/starter/configuration/AdapterConfiguration.java
@@ -1,12 +1,20 @@
 package dev.vality.adapter.bank.spring.boot.starter.configuration;
 
 import dev.vality.adapter.bank.spring.boot.starter.configuration.properties.AdapterProperties;
+import dev.vality.adapter.flow.lib.service.TagManagementService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties({AdapterProperties.class})
 @ConditionalOnExpression("${adapter-bank-spring-boot-starter.adapter.enabled:true}")
 public class AdapterConfiguration {
+
+    @Bean
+    public TagManagementService tagManagementService(
+            dev.vality.adapter.flow.lib.utils.AdapterProperties adapterProperties) {
+        return new TagManagementService(adapterProperties);
+    }
 }
--- src/main/java/dev/vality/adapter/bank/spring/boot/starter/configuration/BenderConfiguration.java
+++ src/main/java/dev/vality/adapter/bank/spring/boot/starter/configuration/BenderConfiguration.java
@@ -2,6 +2,7 @@ package dev.vality.adapter.bank.spring.boot.starter.configuration;
 
 import dev.vality.adapter.bank.spring.boot.starter.configuration.properties.BenderClientProperties;
 import dev.vality.adapter.common.bender.BenderClient;
+import dev.vality.adapter.flow.lib.service.IdGenerator;
 import dev.vality.bender.BenderSrv;
 import dev.vality.woody.thrift.impl.http.THSpawnClientBuilder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
@@ -27,5 +28,10 @@ public class BenderConfiguration {
     @Bean
     public BenderClient benderClient(BenderSrv.Iface benderSrv, BenderClientProperties properties) {
         return new BenderClient(benderSrv, properties.getNamespace());
+    }
+
+    @Bean
+    public IdGenerator idGenerator(BenderSrv.Iface benderSrv) {
+        return new IdGenerator(benderSrv);
     }
 }
